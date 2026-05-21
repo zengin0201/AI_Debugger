cba5420398f4b62ba32a6667a2bbd99b860bc8a7
Alexey Borodin <aleksejborodin711@gmail.com>
2026-05-21 23:00:14 +0300

    Update setup.py

--- python-package/setup.py
+++ python-package/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="zengin-ai-debugger",       # Имя, которое будут писать в pip install
-    version="1.0.1",                 # Версия пакета
+    name="zengin-ai-debugger",      
+    version="1.0.1",                 
     description="Visual real-time debugger for LangChain AI agents",
     author="Alexey Borodin",
-    packages=find_packages(),        # Автоматически найдет папку zengin_ai_debugger
+    packages=find_packages(),       
     install_requires=[
-        "httpx",                     # Твой код использует httpx
-        "langchain-core"             # Твой код использует базовые классы LangChain
+        "httpx",                     
+        "langchain-core"            
     ],
     python_requires=">=3.8",
 )
