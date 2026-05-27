1e3f9c005fa2ae7bcd448ac13cef15714b8ddb0f
Alexey Borodin <aleksejborodin711@gmail.com>
2026-05-27 15:15:27 +0300

    Fix formatting issues in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="zengin_ai_debugger",       # Имя, которое будут писать в pip install
-    version="1.0.7",                 # Версия пакета
+    name="zengin_ai_debugger",       
+    version="1.0.7",               
     description="Visual real-time debugger for LangChain AI agents",
     author="Alexey Borodin",
     packages=["zengin_ai_debugger"], 
-    package_dir={"zengin_ai_debugger": "python-package"},            # Автоматически найдет папку zengin_ai_debugger
+    package_dir={"zengin_ai_debugger": "python-package"},           
     install_requires=[
-        "requests",                     # Твой код использует httpx
-        "langchain-core"             # Твой код использует базовые классы LangChain
+        "requests",                     
+        "langchain-core"             
     ],
     python_requires=">=3.8",
 )
